5e37711812049f82715c20a6d4d30ad16a35e0ff
Vincent Ballandi <vincent.ballandi@epitech.eu>
2021-11-06 11:41:35 +0100

    ADD: Expedition class / setDoc in firestore

--- scripts/expedition_form.js
+++ scripts/expedition_form.js
@@ -1,1 +1,21 @@
-console.log("hello")
+import { doc, setDoc } from "firebase/firestore";
+
+await setDoc(doc(db, "expedition"), {
+    name: "ex_name",
+    date: "ex_date",
+    description: "ex_description"
+});
+
+const expedition_form = doc(db, "expedition");
+setDoc(expedition_form, { capital: true}, {merge: true});
+
+class expedition_class {
+    constructor (name, date, description) {
+        this.name = name;
+        this.date = state;
+        this.description = description;
+    }
+    toString() {
+        return this.name + ', ' + this.date + ', ' + this.description;
+    }
+}
